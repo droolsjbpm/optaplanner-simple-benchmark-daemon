bfa5d451bad366f06a0f0b363e91c3c97bf54e39
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-11-05 11:01:19 +0100

    Fix ssh agent stuff

--- runInBackground.sh
+++ runInBackground.sh
@@ -2,7 +2,9 @@
 
 # Daemon that automatically runs every benchmark config files that is dropped into the local/input
 
-exec ssh-agent bash
-ssh-add
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+  eval `ssh-agent -s`
+  ssh-add
+fi
 ./run.sh & > /dev/null 2> local/errorLog.txt
 
